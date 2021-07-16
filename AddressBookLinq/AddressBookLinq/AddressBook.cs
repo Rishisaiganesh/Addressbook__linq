19bd05c840404c6e85035005857371ab6bd39a1e
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-16 14:14:57 +0530

    Creating Table

---
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLinq
+{
+    public class AddressBook
+    {
+        public void CreateAddressTable()
+        {
+            DataTable table = new DataTable();
+         }
+    }
+}
---
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to addressBook using Linq");
+            AddressBook address = new AddressBook();
+        }
+    }
+}
